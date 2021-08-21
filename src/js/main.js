2ed93329e55bf71495a1b0b32887e266709507c7
Hicabi YILMAZ <yilmazhicabi@gmail.com>
2021-08-22 00:56:14 +0300

    basic code formatting

--- src/js/main.js
+++ src/js/main.js
@@ -1,105 +1,72 @@
 let element1 = document.getElementsByClassName("left-tab");
 let element2 = document.getElementsByClassName("right-tab");
 element1[0].onclick = function () {
-
-    element1[0].classList.add("active");
-    element2[0].classList.remove("active");
-
-
-}
-
+  element1[0].classList.add("active");
+  element2[0].classList.remove("active");
+};
 
 element2[0].onclick = function () {
-    element2[0].classList.add("active");
-    element1[0].classList.remove("active");
-}
+  element2[0].classList.add("active");
+  element1[0].classList.remove("active");
+};
 
 let counter = 2;
 
 let element3 = document.getElementsByClassName("right-arrow");
 element3[0].onclick = function () {
+  if (counter >= 5) {
+    counter = 2;
+  }
 
-    //  console.log(counter);
+  if (counter == 2) {
+    let text1 = document.getElementsByClassName("text-1");
+    let text3 = document.getElementsByClassName("text-3");
 
-    console.log(counter);
-    counter++;
-    console.log(counter);
-    if (counter >= 5) {
-        counter = 2;
-    }
+    text1[0].classList.remove("nothing");
+    text3[0].classList.add("nothing");
+  }
+  if (counter == 3) {
+    let text2 = document.getElementsByClassName("text-2");
+    let text1 = document.getElementsByClassName("text-1");
 
-    if (counter == 2) {
-        let text1 = document.getElementsByClassName("text-1");
-        let text3 = document.getElementsByClassName("text-3");
+    text2[0].classList.remove("nothing");
+    text1[0].classList.add("nothing");
+  }
+  if (counter == 4) {
+    let text3 = document.getElementsByClassName("text-3");
+    let text2 = document.getElementsByClassName("text-2");
 
-        text1[0].classList.remove("nothing");
-        text3[0].classList.add("nothing");
-
-
-    }
-    if (counter == 3) {
-        let text2 = document.getElementsByClassName("text-2");
-        let text1 = document.getElementsByClassName("text-1");
-
-        text2[0].classList.remove("nothing");
-        text1[0].classList.add("nothing");
-
-
-    }
-    if (counter == 4) {
-        let text3 = document.getElementsByClassName("text-3");
-        let text2 = document.getElementsByClassName("text-2");
-
-        text3[0].classList.remove("nothing");
-        text2[0].classList.add("nothing");
-
-
-
-    }
-
-
-
-
-}
-
+    text3[0].classList.remove("nothing");
+    text2[0].classList.add("nothing");
+  }
+};
 
 let element4 = document.getElementsByClassName("left-arrow");
 element4[0].onclick = function () {
+  counter++;
 
-    console.log(counter);
-    counter++;
-    console.log(counter);
-    if (counter >= 5) {
-        counter = 2;
-    }
+  if (counter >= 5) {
+    counter = 2;
+  }
 
-    if (counter == 2) {
-        let text1 = document.getElementsByClassName("text-1");
-        let text3 = document.getElementsByClassName("text-3");
-        text1[0].classList.remove("nothing");
-        text3[0].classList.add("nothing");
+  if (counter == 2) {
+    let text1 = document.getElementsByClassName("text-1");
+    let text3 = document.getElementsByClassName("text-3");
+    text1[0].classList.remove("nothing");
+    text3[0].classList.add("nothing");
+  }
+  if (counter == 3) {
+    let text2 = document.getElementsByClassName("text-2");
+    let text1 = document.getElementsByClassName("text-1");
 
-    }
-    if (counter == 3) {
-        let text2 = document.getElementsByClassName("text-2");
-        let text1 = document.getElementsByClassName("text-1");
+    text2[0].classList.remove("nothing");
+    text1[0].classList.add("nothing");
+  }
+  if (counter == 4) {
+    let text3 = document.getElementsByClassName("text-3");
+    let text2 = document.getElementsByClassName("text-2");
 
-        text2[0].classList.remove("nothing");
-        text1[0].classList.add("nothing");
-        //console.log(textArea);
-
-    }
-    if (counter == 4) {
-        let text3 = document.getElementsByClassName("text-3");
-        let text2 = document.getElementsByClassName("text-2");
-
-        text3[0].classList.remove("nothing");
-        text2[0].classList.add("nothing");
-
-
-    }
-
-
-
-
-}
+    text3[0].classList.remove("nothing");
+    text2[0].classList.add("nothing");
+  }
+};
